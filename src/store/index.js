198f2192ddfc21f5ff023c91a1a75d15e40ff09e
june <mailforjavascript@gmail.com>
2022-09-17 21:15:16 +0700

    add store product.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,7 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    product,
   }
 })
---
+++ src/store/product.js
@@ -0,0 +1,12 @@
+import { createStore } from 'vuex'
+
+export default product({
+    state: {
+    },
+    getters: {
+    },
+    mutations: {
+    },
+    actions: {
+    },
+})
