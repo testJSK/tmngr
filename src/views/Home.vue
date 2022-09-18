a9c70a67db609f952bc31e653ae4c357bcec6517
june <mailforjavascript@gmail.com>
2022-09-18 14:51:12 +0700

    add product api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <h1>Products -{{ products }} -</h1>
+    <h1>Products</h1>
     <div class="row">
       <div class="col col-sm-4 mb-3 mt-3">
-        <div class="card" v-for="product in products" :key="product">
+        <div class="card" v-for="pr in products" :key="pr">
           <div class="card-body" >
-<!--            <h3>{{ prod.productname }}</h3>-->
-<!--            <div>{{ prod.productcost }}</div>-->
+<!--            <h3>{{ pr.title }}</h3>-->
+<!--            <div>{{ pr.price }}</div>-->
             <router-link to="">Read more</router-link>
             <hr>
             <button class="btn btn-success">Remove</button>
