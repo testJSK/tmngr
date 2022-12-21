36af2ba51066ad769b8dc14ce70e4a3b760e5478
testJSK <110838950+testJSK@users.noreply.github.com>
2022-12-21 08:51:56 +0700

    Update http.js

--- src/plugins/http.js
+++ src/plugins/http.js
@@ -4,8 +4,7 @@ import createProductsApi from '@/api/products'
 
 export default () => {
     const http = axios.create({
-        baseURL: 'http://localhost:8080/api/',
-        // baseURL: 'http://faceprog.ru/reactcourseapi/',
+        baseURL: 'http://localhost:8080/api/',        
         timeout: 10000,
     });
 
@@ -17,22 +16,3 @@ export default () => {
 
     return { http, api };
 }
-
-
-// import axios from 'axios'
-// import createProductsApi from '@/api/products'
-// import createCartApi from '@/api/cart'
-//
-// export default () => {
-//     const http = axios.create({
-//         baseURL: 'http://faceprog.ru/reactcourseapi/',
-//         timeout: 10000,
-//     });
-//
-//     const api = {
-//         products: createProductsApi(http),
-//         cart: createCartApi(http)
-//     }
-//
-//     return { http, api };
-// }
